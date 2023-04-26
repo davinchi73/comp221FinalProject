ed8a06793a0c0bc7ae7106d3b2c75fe2eabb23f5
BraydenCoronado <bcoronad@macalester.edu>
2023-04-26 17:59:45 -0500

    merging

--- src/race/Final_Project.java
+++ src/race/Final_Project.java
@@ -1,16 +1,34 @@
 package race;
 
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Point;
+
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Final_Project {
     private static CanvasWindow canvas;
+    private static ArrayList<Dot> listOfDots;
     public static void main(String[] args) {
-
         canvas = new CanvasWindow("Race", 1000, 1000);
+        listOfDots = new ArrayList<>();
+
         Color skyBlue = new Color(135, 206, 235);
         canvas.setBackground(skyBlue);
+<<<<<<< Updated upstream
         
+=======
+
+        canvas.onClick(event -> placeDots(event.getPosition()));
+    }
+
+    private static void placeDots(Point point) {
+>>>>>>> Stashed changes
         
+        Dot dot = new Dot(point.getX(), point.getY(), 5);
+
+        canvas.add(dot);
+        listOfDots.add(point);
+
     }
 }
