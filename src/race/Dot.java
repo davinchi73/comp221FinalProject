b0834142e7a6534c73ff324eaf23e902eb7bbaa1
davinchi73 <dev107c.d133@gmail.com>
2023-04-26 18:00:10 -0500

    color dot

--- src/race/Dot.java
+++ src/race/Dot.java
@@ -2,10 +2,13 @@ package race;
 
 import edu.macalester.graphics.Ellipse;
 
+import java.awt.Color;
+
 public class Dot extends Ellipse {
 
     public Dot(double xpos, double ypos, double radius) {
         super(xpos, ypos, radius, radius);
+        super.setFillColor(Color.BLACK);
     }
     
 }
