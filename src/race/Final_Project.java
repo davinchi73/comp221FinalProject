de3c26670e449333c2906035658f0f10a96ccc05
BraydenCoronado <bcoronad@macalester.edu>
2023-04-26 17:37:00 -0500

    Just added a new file for the projet to be run in

---
+++ src/race/Final_Project.java
@@ -0,0 +1,16 @@
+package race;
+
+import edu.macalester.graphics.CanvasWindow;
+import java.awt.Color;
+
+public class Final_Project {
+    private static CanvasWindow canvas;
+    public static void main(String[] args) {
+
+        canvas = new CanvasWindow("Race", 1000, 1000);
+
+        Color skyBlue = new Color(135, 206, 235);
+        canvas.setBackground(skyBlue);
+
+    }
+}
