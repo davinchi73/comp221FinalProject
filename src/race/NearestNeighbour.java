90e531735ae56b3a3489220db40454b779e1ab9d
malekkaloti <80399146+malekkaloti@users.noreply.github.com>
2023-04-27 20:10:15 -0500

    Add Point and Nearest Neighbor classes

---
+++ src/race/NearestNeighbour.java
@@ -0,0 +1,74 @@
+package race;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+// import edu.macalester.graphics.Point;
+
+public class NearestNeighbour {
+
+
+    public static void main(String[] args) {
+        
+        List<Point> points = Arrays.asList(new Point(1, 0), new Point(2, 1), new Point(3, 2), new Point(4, 2), new Point(5, 2) );
+
+        Point source = points.get(0);
+        Point target = points.get(points.size() - 1);
+        
+        System.out.println(getNearestNeighbourPath(points, source, target));
+    }
+
+
+    /*
+     * Given a list of points, a source, and a target, return the path in a list 
+     * that visits all points using the nearest neighbour algorithm in which a 
+     * greedy choice is made at each step until all points are visited.
+     * 
+     * The path starts at the source and ends at the target.
+     */
+    public static List<Point> getNearestNeighbourPath(List<Point> points, Point source, Point target) {
+        List<Point> path = new ArrayList<>();
+
+        // System.out.println(source);
+        points.remove(source);
+        points.remove(target);
+
+        Point currentPoint = source;
+        path.add(currentPoint);
+        System.out.println(currentPoint);
+
+        while (points.size() > 0) {
+            Point nearestNeighbour = getNearestNeighbour(points, currentPoint);
+            path.add(nearestNeighbour);
+            points.remove(nearestNeighbour);
+            currentPoint = nearestNeighbour;
+        }
+
+        path.add(target);
+        System.out.println(path);
+        return path;
+    }
+
+    /*
+     * Given a list of points and a current point, return the nearest neighbour
+     */
+    private static Point getNearestNeighbour(List<Point> points, Point currentPoint) {
+        Point nearestNeighbour = null;
+        
+        for (Point point : points) {
+            if (point != currentPoint) {
+                if (nearestNeighbour == null) {
+                    nearestNeighbour = point;
+                } else {
+                    if (currentPoint.distance(point) < currentPoint.distance(nearestNeighbour)) {
+                        nearestNeighbour = point;
+                    }
+                }
+            }
+        }
+        return nearestNeighbour;
+    }
+    
+
+
+}
---
+++ src/race/Point.java
@@ -0,0 +1,269 @@
+package race;
+
+import java.awt.geom.Point2D;
+import java.util.Objects;
+
+/**
+ * A point in two-dimensional space.
+ * <p>
+ * This class is immutable. All methods return a new Point.
+ *
+ * @author Paul Cantrell
+ */
+public final class Point {
+    /**
+     * The point (0,0).
+     */
+    public static final Point ORIGIN = new Point(0, 0);
+
+    /**
+     * The point (1,0).
+     */
+    public static final Point UNIT_X = new Point(1, 0);
+
+    /**
+     * The point (0,1).
+     */
+    public static final Point UNIT_Y = new Point(0, 1);
+
+    /**
+     * The point (1,1).
+     */
+    public static final Point ONE_ONE = new Point(1, 1);
+
+    /**
+     * A point at (-∞,-∞).
+     */
+    public static final Point MIN = new Point(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+
+    /**
+     * A point at (∞,∞).
+     */
+    public static final Point MAX = new Point(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+
+    private final double x, y;
+
+    /**
+     * Creates a point with the given horizontal and vertical components.
+     */
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns the unit vector at the given angle in radians.
+     */
+    public static Point atAngle(double theta) {
+        return new Point(Math.cos(theta), Math.sin(theta));
+    }
+
+    /**
+     * The vertical position of this point.
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * The horizontal position of this point.
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * Returns a point with the same y as this one, but a different x.
+     */
+    public Point withX(double x) {
+        return new Point(x, y);
+    }
+
+    /**
+     * Returns a point with the same x as this one, but a different y.
+     */
+    public Point withY(double y) {
+        return new Point(x, y);
+    }
+
+    /**
+     * Returns the angle in radians counterclockwise (or clockwise if you view y as up) from the
+     * x-axis to this point.
+     */
+    public double angle() {
+        return Math.atan2(y, x);
+    }
+
+    /**
+     * Returns the distance from this point to the origin.
+     */
+    public double magnitude() {
+        return Math.hypot(x, y);
+    }
+
+    /**
+     * Returns the distance between this point and another point.
+     */
+    public double distance(Point p) {
+        return Math.hypot(p.x - x, p.y - y);
+    }
+
+    /**
+     * Treats the given point like a displacement vector and adds it to the current point.
+     */
+    public Point add(Point p) {
+        return new Point(x + p.x, y + p.y);
+    }
+
+    /**
+     * Treats p like a displacement vector and subtracts it from the current point
+     */
+    public Point subtract(Point p) {
+        return new Point(x - p.x, y - p.y);
+    }
+
+    /**
+     * Multiplies this point's coordinates by a scalar.
+     */
+    public Point scale(double s) {
+        return scale(s, s);
+    }
+
+    /**
+     * Multiplies x by sx and y by sy.
+     */
+    public Point scale(double sx, double sy) {
+        return new Point(x * sx, y * sy);
+    }
+
+    /**
+     * Returns a point whose x is the minimum of the two given points' x coordinates, and whose y is
+     * the minimum of the two given points' y coordinates.
+     */
+    public static Point min(Point p0, Point p1) {
+        return new Point(
+                Math.min(p0.x, p1.x),
+                Math.min(p0.y, p1.y));
+    }
+
+    /**
+     * Returns a point whose x is the maximum of the two given points' x coordinates, and whose y is
+     * the maximum of the two given points' y coordinates.
+     */
+    public static Point max(Point p0, Point p1) {
+        return new Point(
+                Math.max(p0.x, p1.x),
+                Math.max(p0.y, p1.y));
+    }
+
+    /**
+     * Makes the coordinates of the input point “wrap around” as if moving in a toroidal space
+     * extending from 0 to maxBounds.x, and from 0 to maxBounds.y: coordinates that go off one edge
+     * move to the other edge.
+     * 
+     * Unlike Java’s raw modular arithmetic, this method handles negative values as expected.
+     * 
+     * @param maxBounds The maximum x and y coordinates
+     * @return A new Point whose coordinates are each greater than or equal to zero, and less than
+     *         the coordinates of maxBounds
+     */
+    public Point wrapAround(Point maxBounds) {
+        return wrapAround(Point.ORIGIN, maxBounds);
+    }
+
+    /**
+     * Makes the coordinates of the input point “wrap around” as if moving in a toroidal space:
+     * coordinates that go off one side move to the other side.
+     * 
+     * Unlike Java’s raw modular arithmetic, this method handles negative values as expected.
+     * 
+     * @param minBounds The minimum x and y coordinates
+     * @param maxBounds The maximum x and y coordinates
+     * @return A new Point whose coordinates are each greater than or equal to the coordinates of
+     *         minBounds, and less than the coordinates of maxBounds
+     */
+    public Point wrapAround(Point minBounds, Point maxBounds) {
+        return new Point(
+            wrapAround(x, minBounds.x, maxBounds.x),
+            wrapAround(y, minBounds.y, maxBounds.y));
+    }
+
+    private static double wrapAround(double x, double x0, double x1) {
+        double range = x1 - x0;
+        return ((x - x0) % range + range) % range + x0;
+    }
+
+    /**
+     * Returns the point rotated around the origin by the given angle in radians.
+     */
+    public Point rotate(double angle) {
+        return new Point(
+                getX() * Math.cos(angle) - getY() * Math.sin(angle),
+                getX() * Math.sin(angle) + getY() * Math.cos(angle));
+    }
+
+    /**
+     * Returns the point rotated around a given axis point by the given angle in radians.
+     */
+    public Point rotate(double angle, Point center) {
+        return subtract(center)
+                .rotate(angle)
+                .add(center);
+    }
+
+    /**
+     * Linearly interpolates by alpha between the two points.
+     *
+     * @param p0    The point for alpha = 0
+     * @param p1    The point for alpha = 1
+     * @param alpha The continuum from p0 to p1
+     */
+    public static Point interpolate(Point p0, Point p1, double alpha) {
+        return p0.add(
+                p1.subtract(p0).scale(alpha));
+    }
+
+    /**
+     * Two points are equal if they have the same coordinates.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point that = (Point) o;
+        return Double.compare(this.x, that.x) == 0
+            && Double.compare(this.y, that.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    // ------ Java AWT Compatibility
+
+    /**
+     * Converts a Java AWT Point2D to a kilt-graphics Point.
+     */
+    public Point(Point2D point) {
+        this(point.getX(), point.getY());
+    }
+
+    /**
+     * Converts this point to the Java AWT Point2D.
+     */
+    public Point2D.Double toPoint2D() {
+        return new Point2D.Double(x, y);
+    }
+}
+
